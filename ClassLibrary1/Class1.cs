8ef2005254c9fe1c9eb96c26ba93cdbc13ad91fc
kseniailina <kse1920@yandex.ru>
2019-10-04 16:44:46 +0300

    test

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Assert = Xunit.Assert;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+
+        [Test]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Test]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
