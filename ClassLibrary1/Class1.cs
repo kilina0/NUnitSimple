1fbcbffda468c9be0b4da4b561738ddebfc990da
kseniailina <kse1920@yandex.ru>
2019-10-04 17:37:00 +0300

    tests

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -24,9 +24,16 @@ namespace ClassLibrary1
         }
 		
 		[Test]
-        public void Blablabla()
+        public void FailingTest2()
         {
             Assert.Equal(5, Add(7, 2));
+        }
+
+        [Test]
+        public void FailingTest3()
+        {
+            Console.WriteLine("123456");
+            Assert.Equal("test", "test2");
         }
 
         int Add(int x, int y)
